b4a23b3aac349850774036e3b7cc924083f3a5f1
Owen Nursalim <owennoah102@gmail.com>
2024-07-26 16:11:03 +0930

    add hobby component

--- src/components/Hobby.js
+++ src/components/Hobby.js
@@ -8,7 +8,7 @@ export default function Hobby() {
     return <section id="hobbys" className="text-gray-400 bg-gray-900 body-font">
     <div className="container px-5 py-10 mx-auto text-center lg:px-40">
       <div className="flex flex-col w-full mb-20">
-        <BadgeCheckIcon className="text-blue-400 mx-auto inline-block w-10 mb-4" />
+        <BadgeCheckIcon className="text-purple-400 mx-auto inline-block w-10 mb-4" />
         <h1 className="sm:text-4xl text-3xl font-medium title-font mb-4 text-white">
           My Hobbies
           <p className="lg:w-2/3 mx-auto leading-relaxed text-base">
